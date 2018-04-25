e6676130cfc27e936bd1362182c95551fb89473d
sp <spirityy109@gmail.com>
2018-04-25 18:35:27 +0800

    add editor

--- src/Editor/Editor.js
+++ src/Editor/Editor.js
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/json';
+import 'brace/theme/textmate';
+
 class Editor extends Component {
+  Change(newValue) {
+    console.log('change', newValue);
+  }
   render() {
-    return (
-        <div className="editor">
-          <textarea></textarea>
-        </div>
-    );
+    return (<div className="editor">
+      <AceEditor mode="json" theme="textmate" onChange={this.Change} height="100%" name="editor" />
+    </div>);
   }
 }
 
